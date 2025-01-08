6e8bd99e792aed72e554e2539405bdd4f344ab8b
Anton Chan <106675165+chanankuan@users.noreply.github.com>
2025-01-08 18:14:49 +0100

    import and configure multer module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,22 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
+import { MulterModule } from '@nestjs/platform-express';
+import { memoryStorage } from 'multer';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
+import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  // importing MulterModule and use memory storage to use the buffer within the pipe
+  imports: [
+    MulterModule.register({
+      storage: memoryStorage(),
+    }),
+    TypeOrmModule.forFeature([User]),
+    CloudinaryModule,
+  ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, CloudinaryService],
   exports: [UsersService],
 })
 export class UsersModule {}
