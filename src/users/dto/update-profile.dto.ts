b70e5e758b7a023bbf1f9870f96f2bcc391c1a8b
Anton Chan <106675165+chanankuan@users.noreply.github.com>
2025-01-19 18:47:34 +0100

    add update profile dto

--- src/users/dto/update-profile.dto.ts
+++ src/users/dto/update-profile.dto.ts
@@ -2,10 +2,11 @@ import { registerSchema } from 'src/auth/dto';
 import { z } from 'zod';
 
 export const updateProfileSchema = registerSchema
+  .pick({ name: true })
   .partial()
   .strict() // Ensure no extra fields are allowed;
   .refine((data) => Object.values(data).some((value) => value !== undefined), {
-    message: `At least one of the following fields must be provided: ${Object.keys(registerSchema.shape).join(', ')}`,
+    message: `At least one of the following fields must be provided: ${Object.keys(registerSchema.pick({ name: true }).shape).join(', ')}`,
     // path: [], // General error path
   });
 
