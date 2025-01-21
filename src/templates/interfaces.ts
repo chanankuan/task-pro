ca663f1977f83400de0c9d893523e5f56f51cc84
Anton Chan <106675165+chanankuan@users.noreply.github.com>
2025-01-21 09:39:43 +0100

    update: add new contex

--- src/templates/interfaces.ts
+++ src/templates/interfaces.ts
@@ -20,3 +20,21 @@ export interface WelcomeMessageContext {
   supportLink: string;
   year: number;
 }
+
+export interface SupportContext {
+  caseId: string;
+  userEmail: string;
+  description: string;
+  privacyPolicyLink: string;
+  supportLink: string;
+  year: number;
+}
+
+export interface SupportReceiptContext {
+  caseId: string;
+  userName: string;
+  description: string;
+  privacyPolicyLink: string;
+  supportLink: string;
+  year: number;
+}
